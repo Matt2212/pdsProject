c2694efce8a8f48b4e753f745268a0e29738a5b8
mattia <mattiacaracciolo1@gmail.com>
2021-12-23 20:16:44 +0100

    pt_get_frame_from_page pt_load_frame_from_swap scritte

--- os161-base-2.0.3/kern/include/pt.h
+++ os161-base-2.0.3/kern/include/pt.h
@@ -1,11 +1,11 @@
 #ifndef PT_H
 #define PT_H
 
-#include <types.h>
+
 #define TABLE_SIZE 1024
 
-#define GET_EXT_INDEX(vaddr_t addr) (addr >> 22)
-#define GET_INT_INDEX(vaddr_t addr) ((addr & 0x3f000) >> 12)
+#define GET_EXT_INDEX(addr) ( addr >> 22)
+#define GET_INT_INDEX(addr) (( addr & 0x3f000) >> 12)
 typedef struct
 {
     unsigned int frame_no:20;
@@ -20,17 +20,17 @@ typedef struct
 } pt;
 
 
-static unsigned int get_victim(pt* table);
+static vaddr_t get_victim(pt* table); //indirizzo logico nello user space di una pagina indicata come vittima
 
 void init_rows(pt* table, unsigned int index); //creazione e inizializza un blocco di 1024 entries
 
-unsigned int pt_get_frame_from_page(pt* table, vaddr_t addr); //restituisce l'indice del frame associato alla pagina, il frame potrebbe trovarsi in memoira o im swap
+unsigned int pt_get_frame_from_page(pt* table, vaddr_t addr); // da richiamre solo su frame correttamente caricati o in swap restituisce l'indice del frame associato alla pagina, il frame potrebbe trovarsi in memoria o im swap
 
-void pt_load_frame_from_swap(pt* table, vaddr_t frame); //effettua lo swap tra un frame in memoria (swap out) e un frame nel file di swap (swap-in)
+void pt_load_frame_from_swap(pt* table, vaddr_t requested); //effettua lo swap tra un frame in memoria (swap out) e un frame nel file di swap (swap-in)
 
 void pt_load_frame_from_file(pt* table, vaddr_t userptr,vaddr_t frame); //carica un frame da disco e lo scrive nel frame di con logico del kernel addr
 
-void pt_load_free_frame(pt* table, vaddr_t userptr)
+void pt_load_free_frame(pt* table, vaddr_t userptr);
 
 void pt_destroy(pt* table); // distrugge la pagetable
 
--- os161-base-2.0.3/kern/vm/pt.c
+++ os161-base-2.0.3/kern/vm/pt.c
@@ -26,6 +26,35 @@ void init_rows(pt* table, unsigned int index) {
         panic("No space left on the device");
     }
     for(; i < TABLE_SIZE; i++){ // forse inutile
-        table->table[index][i].valid = 0; // basta invalidare l' entry per far perdere di significato tutto il resto
+        table->table[index][i].valid = false; // basta invalidare l' entry per far perdere di significato tutto il resto
     }
+}
+
+// ritorna 0 in caso di errori
+unsigned int pt_get_frame_from_page(pt* table, vaddr_t addr) {
+    unsigned int exte, inte;
+    exte = GET_EXT_INDEX(addr);
+    inte = GET_INT_INDEX(addr);
+    if (table->table[exte] == NULL || !table->table[exte][inte].valid) return 0;
+    if (table->table[exte][inte].swp){
+        // TODO chied se meglio prendere le pagine libere per questo swapout
+        pt_load_frame_from_swap(table, addr);
+    }
+
+    return table->table[exte][inte].frame_no;
+}
+
+void pt_load_frame_from_swap(pt* table, vaddr_t requested) {
+    unsigned int exte, inte, vic_int, vic_ext;
+    exte = GET_EXT_INDEX(requested);
+    inte = GET_INT_INDEX(requested);
+    vaddr_t victim = get_victim(table);
+    vic_int = GET_INT_INDEX(victim);  
+    vic_ext = GET_EXT_INDEX(victim);
+    paddr_t frame_victim = table->table[vic_ext][vic_int].frame_no * PAGE_SIZE; //indirizzo fisco del frame vittima
+    table->table[vic_ext][vic_int].frame_no = swap_set(victim);
+    table->table[vic_ext][vic_int].swp = true;
+    swap_get(PADDR_TO_KVADDR(frame_victim), table->table[exte][inte].frame_no); // scrivo sul frame victim il contenuto del frame in posizione frame_no presente nel file di swap
+    table->table[exte][inte].frame_no = frame_victim / PAGE_SIZE;
+    table->table[exte][inte].swp = false;
 }

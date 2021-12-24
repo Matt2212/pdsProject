00dd69e48f32a95b37d42bce9dc2149a56b6c1d9
mattia <mattiacaracciolo1@gmail.com>
2021-12-24 15:42:47 +0100

    added get victim and  pt _load_free_frame

--- os161-base-2.0.3/kern/include/pt.h
+++ os161-base-2.0.3/kern/include/pt.h
@@ -6,6 +6,8 @@
 
 #define GET_EXT_INDEX(addr) ( addr >> 22)
 #define GET_INT_INDEX(addr) (( addr & 0x3f000) >> 12)
+
+#define MACRO_PAGE_SIZE 1 << 22
 typedef struct
 {
     unsigned int frame_no:20;
@@ -22,7 +24,7 @@ typedef struct
 
 static vaddr_t get_victim(pt* table); //indirizzo logico nello user space di una pagina indicata come vittima
 
-void init_rows(pt* table, unsigned int index); //creazione e inizializza un blocco di 1024 entries
+void init_rows(pt* table, unsigned int index); //creazione e inizializzazione di un blocco di 1024 entries
 
 unsigned int pt_get_frame_from_page(pt* table, vaddr_t addr); // da richiamre solo su frame correttamente caricati o in swap restituisce l'indice del frame associato alla pagina, il frame potrebbe trovarsi in memoria o im swap
 
@@ -30,7 +32,7 @@ void pt_load_frame_from_swap(pt* table, vaddr_t requested); //effettua lo swap t
 
 void pt_load_frame_from_file(pt* table, vaddr_t userptr,vaddr_t frame); //carica un frame da disco e lo scrive nel frame di con logico del kernel addr
 
-void pt_load_free_frame(pt* table, vaddr_t userptr);
+bool pt_load_free_frame(pt* table, vaddr_t userptr); //preleva un frame libero dalla lista dei frame liberi. Ritorna true se tutto va bene, false altrimenti
 
 void pt_destroy(pt* table); // distrugge la pagetable
 
--- os161-base-2.0.3/kern/vm/pt.c
+++ os161-base-2.0.3/kern/vm/pt.c
@@ -2,6 +2,31 @@
 #include <coremap.h>
 #include <swapfile.h>
 
+#define PAGE_NOT_FOUND 1
+
+vaddr_t get_victim(pt* table ) {
+    static vaddr_t victim = 0;
+    vaddr_t ret = victim;
+    int count = 0;
+
+    while ( table->table[GET_EXT_INDEX(ret)] != NULL && count < 1024 ) {
+        ret += MACRO_PAGE_SIZE;
+    }
+
+    if (count >= 1024) return PAGE_NOT_FOUND; // ritorno un indirizzo che non può rappresentare una pagina
+
+    while ( !table->table[GET_EXT_INDEX(ret)][GET_INT_INDEX(ret)].valid || 
+            table->table[GET_EXT_INDEX(ret)][GET_INT_INDEX(ret)].swp ) {
+        ret += PAGE_SIZE;
+        if ( ret == victim ) return PAGE_NOT_FOUND;
+        while ( table->table[GET_EXT_INDEX(ret)] != NULL )
+            ret += MACRO_PAGE_SIZE;
+    }
+
+    victim = ret;
+    return victim;
+}
+
 void pt_destroy(pt* table){
     int i = 0;
     for(;i < TABLE_SIZE; i++) {
@@ -49,6 +74,8 @@ void pt_load_frame_from_swap(pt* table, vaddr_t requested) {
     exte = GET_EXT_INDEX(requested);
     inte = GET_INT_INDEX(requested);
     vaddr_t victim = get_victim(table);
+    if (victim == PAGE_NOT_FOUND)
+        panic("no space ")
     vic_int = GET_INT_INDEX(victim);  
     vic_ext = GET_EXT_INDEX(victim);
     paddr_t frame_victim = table->table[vic_ext][vic_int].frame_no * PAGE_SIZE; //indirizzo fisco del frame vittima
@@ -57,4 +84,13 @@ void pt_load_frame_from_swap(pt* table, vaddr_t requested) {
     swap_get(PADDR_TO_KVADDR(frame_victim), table->table[exte][inte].frame_no); // scrivo sul frame victim il contenuto del frame in posizione frame_no presente nel file di swap
     table->table[exte][inte].frame_no = frame_victim / PAGE_SIZE;
     table->table[exte][inte].swp = false;
+}
+
+bool pt_load_free_frame(pt* table, vaddr_t userptr) {
+    unsigned int exte, inte;
+    exte = GET_EXT_INDEX(userptr);
+    inte = GET_INT_INDEX(userptr);
+    if (!(table->table[exte][inte].frame_no = get_frame()))
+        return false;
+    table->table[exte][inte].valid = true;
 }
